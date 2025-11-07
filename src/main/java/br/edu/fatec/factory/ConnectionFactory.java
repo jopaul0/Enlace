cd70eb13efdd8b8669c2bd7a250104bececbd920
João Paulo <jopaulo.as8@gmail.com>
2025-11-07 11:18:53 -0300

    feat(database): Add ConnectionFactory

---
+++ src/main/java/br/edu/fatec/factory/ConnectionFactory.java
@@ -0,0 +1,23 @@
+package br.edu.fatec.factory;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+    private static final Dotenv dotenv = Dotenv.load();
+
+    private static final String URL = dotenv.get("DB_URL");
+    private static final String USER = dotenv.get("DB_USER");
+    private static final String PASSWORD = dotenv.get("DB_PASSWORD");
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao conectar ao banco de dados", e);
+        }
+    }
+}
