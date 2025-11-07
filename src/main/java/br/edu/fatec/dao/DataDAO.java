8397006c47ccb25dce5fe523397171686b94eee0
João Paulo <jopaulo.as8@gmail.com>
2025-11-07 18:49:49 -0300

    feat(database): Add DataDao (generic dao)

---
+++ src/main/java/br/edu/fatec/dao/DataDAO.java
@@ -0,0 +1,48 @@
+package br.edu.fatec.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import static br.edu.fatec.factory.ConnectionFactory.getConnection;
+
+public abstract class DataDAO<T> {
+    protected void executeUpdate(String sql, Object... params) throws SQLException {
+        try (Connection conn = getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            setParameters(ps, params);
+            ps.executeUpdate();
+        }
+    }
+
+    protected List<T> executeQuery(String sql, Object... params) throws SQLException {
+        try (Connection conn = getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            setParameters(ps, params);
+
+            try (ResultSet rs = ps.executeQuery()) {
+                return mapResultSetToList(rs);
+            }
+        }
+    }
+
+    private void setParameters(PreparedStatement ps, Object... params) throws SQLException {
+        for (int i = 0; i < params.length; i++) {
+            ps.setObject(i + 1, params[i]);
+        }
+    }
+
+    protected abstract List<T> mapResultSetToList(ResultSet rs) throws SQLException;
+
+    public T findById(int id) throws SQLException {
+        String sql = "SELECT * FROM " + getTableName() + " WHERE id = ?";
+        List<T> results = executeQuery(sql, id);
+        return results.isEmpty() ? null : results.get(0);
+    }
+
+    protected abstract String getTableName();
+}
