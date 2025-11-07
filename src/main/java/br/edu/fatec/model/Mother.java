dec28bd49a4a127281ed8d96500b59e674ac133c
João Paulo <jopaulo.as8@gmail.com>
2025-11-07 12:20:21 -0300

    feat(mother): Add mother model

---
+++ src/main/java/br/edu/fatec/model/Mother.java
@@ -0,0 +1,19 @@
+package br.edu.fatec.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Mother {
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+}
