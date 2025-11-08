668d54f83c59608efd598e0ef952c683e6e8615e
João Paulo <jopaulo.as8@gmail.com>
2025-11-08 09:40:13 -0300

    test(mother): Test class mother dao

--- src/main/java/br/edu/fatec/dao/TestDAO.java
+++ src/main/java/br/edu/fatec/dao/TestDAO.java
@@ -1,4 +1,20 @@
 package br.edu.fatec.dao;
 
+import br.edu.fatec.model.Mother;
+
+import java.sql.SQLException;
+
 public class TestDAO {
+    public static void main(String[] args) throws SQLException {
+        Mother mae = new Mother();
+        mae.setId((long) 1);
+        mae.setName("Vanessa");
+        mae.setCpf("111111111111");
+        mae.setEmail("pelega@hotmail.com");
+        mae.setPhone("12992196356");
+
+        MotherDAO maedao = new MotherDAO();
+
+        maedao.delete(mae);
+    }
 }
