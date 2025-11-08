5f5a720c9ae9e5bb677bb15991d92cd221f2f920
João Paulo <jopaulo.as8@gmail.com>
2025-11-08 10:26:59 -0300

    chore(mother): Update mother's atributes

--- src/main/java/br/edu/fatec/model/Mother.java
+++ src/main/java/br/edu/fatec/model/Mother.java
@@ -1,10 +1,13 @@
 package br.edu.fatec.model;
 
+import br.edu.fatec.enums.DefaultStatus;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import lombok.ToString;
+
+import java.time.LocalDate;
 
 @Getter
 @Setter
@@ -17,4 +20,6 @@ public class Mother {
     private String cpf;
     private String email;
     private String phone;
+    private LocalDate birthday;
+    private DefaultStatus status;
 }
