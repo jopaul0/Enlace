b257c7beef0ed07a9c482310a55b6c368dbed92f
João Paulo <jopaulo.as8@gmail.com>
2025-11-07 21:12:39 -0300

    feat(service): Add service model

---
+++ src/main/java/br/edu/fatec/model/Service.java
@@ -0,0 +1,18 @@
+package br.edu.fatec.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Services {
+    private Long id;
+    private String name;
+    private String description;
+}
