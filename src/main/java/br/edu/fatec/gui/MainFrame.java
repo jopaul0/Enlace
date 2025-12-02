7c52ae77edb0335658654aef97e87c9f8d9ab353
João Paulo <jopaulo.as8@gmail.com>
2025-12-02 16:06:18 -0300

    feat(view): Add main frame

---
+++ src/main/java/br/edu/fatec/gui/MainFrame.java
@@ -0,0 +1,24 @@
+package br.edu.fatec.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+
+    public MainFrame() {
+        setTitle("Sistema de Gerenciamento - Enlace");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800, 600);
+        setLocationRelativeTo(null);
+
+        JTabbedPane tabbedPane = new JTabbedPane();
+
+        getContentPane().add(tabbedPane, BorderLayout.CENTER);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new MainFrame().setVisible(true);
+        });
+    }
+}
